076c1de3498f19ed91c609146bbb086a0e019fca
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-07 16:03:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -6,7 +6,7 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
   def self.column_names
-    DB[:conn].results_as_hash=true 
+    DB[:conn].results_as_hash=true
     table_info=DB[:conn].execute("pragma table_info('#{table_name}')")
     column_names=[]
     table_info.each do |row|
